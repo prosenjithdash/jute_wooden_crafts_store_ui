030ad30e7e0f5e3d1cee01eb2e4ec9bc0b073f78
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2024-07-04 01:45:33 +0600

    Updated

--- src/page/MyArtCraftList.jsx
+++ src/page/MyArtCraftList.jsx
@@ -1,58 +1,68 @@
+import { useEffect } from "react";
+import useAuth from "../hooks/useAuth";
 
 const MyArtCraftList = () => {
-    return (
-        <div className="overflow-x-auto max-w-[1280px] mx-auto">
-            <table className="table">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Item Name</th>
-                        <th>Rating</th>
-                        <th>Price</th>
-                        <th>Details</th>
-                        <th></th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* row 1 */}
-                    <tr>
-                        {/* <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th> */}
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle h-12 w-12">
-                                        <img
-                                            src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
-                                            alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Hart Hagerty</div>
-                                    <div className="text-sm opacity-50">United States</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Zemlak, Daniel and Leannon
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                        </td>
-                        <td>Purple</td>
-                        <th>
-                            <button className="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
+    const { user } = useAuth() || {}
+    console.log(user)
+
+    useEffect(() => {
+        fetch(`https://jute-wooden-crafts-store-server.vercel.app/myArtCrafts/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+            console.log(data)
+        })
+    },[user])
+    return (
+        // <div className="overflow-x-auto max-w-[1280px] mx-auto">
+        //     <table className="table">
+        //         <thead>
+        //             <tr>
+        //                 <th>Image</th>
+        //                 <th>Item Name</th>
+        //                 <th>Rating</th>
+        //                 <th>Price</th>
+        //                 <th>Price</th>
+        //                 <th></th>
+
+        //             </tr>
+        //         </thead>
+        //         <tbody>
+        //             <tr>
+                   
+        //                 <td>
+        //                     <div className="flex items-center gap-3">
+        //                         <div className="avatar">
+        //                             <div className="mask mask-squircle h-12 w-12">
+        //                                 <img
+        //                                     src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
+        //                                     alt="Avatar Tailwind CSS Component" />
+        //                             </div>
+        //                         </div>
+        //                         <div>
+        //                             <div className="font-bold">Hart Hagerty</div>
+        //                             <div className="text-sm opacity-50">United States</div>
+        //                         </div>
+        //                     </div>
+        //                 </td>
+        //                 <td>
+        //                     Zemlak, Daniel and Leannon
+        //                     <br />
+        //                     <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        //                 </td>
+        //                 <td>Purple</td>
+        //                 <th>
+        //                     <button className="btn btn-ghost btn-xs">details</button>
+        //                 </th>
+        //             </tr>
                    
                     
-                </tbody>
+        //         </tbody>
                
-            </table>
+        //     </table>
+        // </div>
+        <div>
+
         </div>
     );
 };
